d9df6d1a4c514cf89d86a4dd836983bd74ac5f3a
shuxin852 <shuxin0215@gmail.com>
2023-06-13 17:33:28 +0900

    FactoryBotのemailをFakerに変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname { 'test' }
-    email { 'test@test' }
+    email { Faker::Internet.email }
     password { 'aaa111' }
     password_confirmation { password }
     family_name { '山田' }
