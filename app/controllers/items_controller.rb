0582c1082f8816c370c200abfbf739dae28dbd87
shuxin852 <shuxin0215@gmail.com>
2023-06-14 12:23:33 +0900

    itemのDB保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
+    @item.user = current_user
     if @item.save
       redirect_to root_path
     else
