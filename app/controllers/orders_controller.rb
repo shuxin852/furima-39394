2cb0ae67159b9a7d578ccb51b295a91963afb46d
shuxin852 <shuxin0215@gmail.com>
2023-06-19 18:04:46 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_form_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -21,6 +22,15 @@ class OrdersController < ApplicationController
   end
   
   def order_form_params
-    params.require(:order_form).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:order_form).permit(:post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,13 +1,14 @@
 class OrderForm
   include ActiveModel::Model 
   
-  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :municipalities, :address, :building_name, :phone_number, :token
   
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipalities
     validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Phone number should be 10-11 digits" }
+    validates :token
   end
     validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
