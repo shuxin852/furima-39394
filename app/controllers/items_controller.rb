188a3c060bb9b0c0f59d0c88942cc3d72e7ba6a8
shuxin852 <shuxin0215@gmail.com>
2023-06-20 13:55:40 +0900

    売却済商品は編集できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,9 @@ class ItemsController < ApplicationController
     unless @item.user == current_user
       redirect_to root_path
     end
+    if @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
